dda433dc9df4218d4715ae24345e3c45973eb518
kylechung10 <kylechung7@gmail.com>
2021-02-08 02:19:19 -0500

    closes #1

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ let quotes = [
     `"Live as if you were to die tomorrow. Learn as if you were to live forever."
     -Mahatma Gandhi`,
     `"Believe you can and you’re halfway there."
-    -Theodore Roosevelt`
+    -Theodore Roosevelt`,
+    `"If you spend too much time thinking about a thing, you’ll never get it done."
+    -Bruce Lee`,
+    `"Life is a question and how we live it is our answer."
+    -Gary Keller`
 ]
 
-let generate = Math.floor(Math.random() * 3);
+let generate = Math.floor(Math.random() * 5);
 
 console.log(quotes[generate]);
